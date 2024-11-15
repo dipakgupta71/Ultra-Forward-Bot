a12efae3ca31934a6de8ce8ec4935601eab68a56
dipakgupta71 <97516714+dipakgupta71@users.noreply.github.com>
2024-11-15 17:34:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,13 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID = os.environ.get("API_ID", "5128432")
+    API_HASH = os.environ.get("API_HASH", "5436f836f2887d3e680628ff1010e949")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7602790697:AAH2NqK2gu2suk_ZTsUZ-jvZp4jcY3A322s") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://DOXDKGBOT:9pqvIrVMgpqdMsmf@doxdkgbot.klwdk.mongodb.net/?retryWrites=true&w=majority&appName=DOXDKGBOT")
+    DB_NAME = os.environ.get("DB_NAME", "DOXDKGBOT")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '1440378158').split()]
 
 
 class temp(object): 
